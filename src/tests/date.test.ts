2ab2f95fc219219a1a064903a4aea210f333e11b
Jorge Monge <jlo4321jl@gmail.com>
2022-02-26 22:13:33 -0600

    ✅ Add test for getNextDate

--- src/tests/date.test.ts
+++ src/tests/date.test.ts
@@ -1,4 +1,9 @@
-import { daysDiff } from "../functions/date";
+import { DateTime } from "luxon";
+
+import { daysDiff, getNextDate } from "../functions/date";
+import holidays from "../holidays.json";
+
+let arrDates = Object.keys(holidays).map(x => DateTime.fromISO(x))
 
 describe('Testing dayDiff function',()=>{
 	test('should return 1 day of diff',()=>{
@@ -11,5 +16,11 @@ describe('Testing dayDiff function',()=>{
 
 	test('should return 0 day of diff',()=>{
 		expect(daysDiff("2022-02-20","2022-02-20")).toBe(0)
+	})
+})
+
+describe('Testing getNextDate function',()=>{
+	test('should return 2022-05-10',()=>{
+		expect(getNextDate(DateTime.fromISO("2022-05-04"),arrDates)).toBe("2022-05-10")
 	})
 })
